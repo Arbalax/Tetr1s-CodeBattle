e6591f4c09b33f35e23a4eb791c68bcd864606d7
Илья <ilyalapkin1990@gmail.com>
2020-12-05 01:15:05 +0300

    Add AIResultHandler

---
+++ src/main/java/com/codenjoy/dojo/services/AIResultHandler.java
@@ -0,0 +1,40 @@
+package com.codenjoy.dojo.services;
+
+import com.codenjoy.dojo.tetris.client.Board;
+import com.codenjoy.dojo.tetris.model.Result;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static com.codenjoy.dojo.services.Command.*;
+
+public class AIResultHandler {
+    public static List<Command> getCommands(Result result, Board board) {
+        List<Command> commands = new LinkedList<>();
+        int x = board.getCurrentFigurePoint().getX();
+
+        if (result.getX() < x) {
+            for (int i = 0; i < Math.abs(result.getX() - x); i++) {
+                commands.add(LEFT);
+            }
+        }
+        if (result.getX() > x) {
+            for (int i = 0; i < Math.abs(result.getX() - x); i++) {
+                commands.add(RIGHT);
+            }
+        }
+
+        if (result.getRotation() == 0) {
+            commands.add(ROTATE_CLOCKWISE_90);
+        }
+        if (result.getRotation() == 0) {
+            commands.add(ROTATE_CLOCKWISE_180);
+        }
+        if (result.getRotation() == 0) {
+            commands.add(ROTATE_CLOCKWISE_270);
+        }
+
+        return commands;
+    }
+
+}
