77a36fe0268bc931bd174ce469dba315d98a0ead
Илья <ilyalapkin1990@gmail.com>
2020-12-05 00:16:38 +0300

    Add Result entity

---
+++ src/main/java/com/codenjoy/dojo/tetris/model/Result.java
@@ -0,0 +1,12 @@
+package com.codenjoy.dojo.tetris.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Result {
+    private int rotation;
+    private int x;
+}
