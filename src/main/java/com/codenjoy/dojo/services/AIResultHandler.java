739029ee944e3dbaf9012d77d24d902e73024fc2
Илья <ilyalapkin1990@gmail.com>
2020-12-05 01:15:33 +0300

    AIResultHandler copypaste fix

--- src/main/java/com/codenjoy/dojo/services/AIResultHandler.java
+++ src/main/java/com/codenjoy/dojo/services/AIResultHandler.java
@@ -27,10 +27,10 @@ public class AIResultHandler {
         if (result.getRotation() == 0) {
             commands.add(ROTATE_CLOCKWISE_90);
         }
-        if (result.getRotation() == 0) {
+        if (result.getRotation() == 1) {
             commands.add(ROTATE_CLOCKWISE_180);
         }
-        if (result.getRotation() == 0) {
+        if (result.getRotation() == 2) {
             commands.add(ROTATE_CLOCKWISE_270);
         }
 
